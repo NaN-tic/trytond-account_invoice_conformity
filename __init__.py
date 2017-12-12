7ce8f92bd2bf02909b3287c5a8904e1a89e60e14
Raimon Esteve <raimon@nan-tic.com>
2017-12-12 18:40:21 +0100

    Register classes

--- __init__.py
+++ __init__.py
@@ -1,14 +1,13 @@
 #The COPYRIGHT file at the top level of this repository contains the full
 #copyright notices and license terms.
-
 from trytond.pool import Pool
-from .invoice import *
+from . import invoice
 
 
 def register():
     Pool.register(
-        Configuration,
-        ConformGroup,
-        ConformGroupUser,
-        Invoice,
+        invoice.Configuration,
+        invoice.ConformGroup,
+        invoice.ConformGroupUser,
+        invoice.Invoice,
         module='account_invoice_conformity', type_='model')
